db393bf8b15bb2b419f1785568791e2929fa047d
nico <cosmonicob@gmail.com>
2025-04-09 17:39:44 +0200

    Añadir heuristicas

--- modulos/Heuristicas.py
+++ modulos/Heuristicas.py
@@ -11,7 +11,12 @@ class Heuristica:
         return math.sqrt((pos1 - fila_s)**2 + (pos2 - columna_s)**2)
 
     def personal(self,pos1,pos2,columna_s,fila_s):
-        return abs(fila_s - pos1) + abs(columna_s - pos2) + 0.1 * max(abs(fila_s - pos1), abs(columna_s - pos2))  #manhattan pero penalizq alejarse o desviarse de la meta premia ir en linea recta
+        #return abs(fila_s - pos1) + abs(columna_s - pos2) + 0.1 * max(abs(fila_s - pos1), abs(columna_s - pos2))  #manhattan pero penalizq alejarse o desviarse de la meta premia ir en linea recta
+        #return math.sqrt((pos1 - fila_s)**2 + (pos2 - columna_s)**2) * 1.5
+        #return 1.5 * (abs(fila_s - pos1) + abs(columna_s - pos2)) + 0.1 * max(abs(fila_s - pos1), abs(columna_s - pos2))
+        #return math.sqrt((pos1 - fila_s)**2 + (pos2 - columna_s)**2) * 1.5 + 0.1 * max(abs(fila_s - pos1), abs(columna_s - pos2))
+
+
 
     def devolverHeuristica(self,pos1,pos2,columna_s,fila_s):
         if self.nombre == "Manhattan":
